b20639cd523af0c112fc5ba6b4acba6f6995375c
Yerco <yerco@optmail.com>
2022-02-14 17:29:01 -0300

    Detectando user agent

--- api.py
+++ api.py
@@ -3,11 +3,27 @@
 from webob import Request, Response
 
 class API:
+    def __init__(self) -> None:
+        self.routes = {}
+    
     def __call__(self, environ, start_response):
         request = Request(environ)
 
-        response = Response()
-        response.text = "Hola Mundo"
+        response = self.handle_request(request)
 
         return response(environ, start_response)
 
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        
+        return wrapper
+
+    def handle_request(self, request):
+        user_agent= request.environ.get("HTTP_USER_AGENT", "No user agent found.")
+
+        response = Response()
+        response.text = f"Hola, my friend with this user agent: {user_agent}"
+
+        return response
--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from api import API
 
 app = API()
 
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the home page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the about page"
