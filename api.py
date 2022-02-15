d926826e6cde6a8d963eb08611dd7f5410f93648
Yerco <yerco@optmail.com>
2022-02-15 14:28:34 -0300

    Handlers for routes and parsing URL arguments

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 # api.py
 
+from parse import parse
 from webob import Request, Response
 
 class API:
@@ -24,19 +25,21 @@ class API:
         response.status_code = 404
         response.text = "Not found"
 
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+        return None, None
+
     def handle_request(self, request):
         response = Response()
         
-        handler = self.find_handler(request_path=request.path)
+        handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
-            handler(request, response)
+            handler(request, response, **kwargs)
         else:
             self.default_response(response)
         
         return response
-
-    def find_handler(self, request_path):
-        for path, handler in self.routes.items():
-            if path == request_path:
-                return handler
--- app.py
+++ app.py
@@ -11,3 +11,16 @@ def home(request, response):
 @app.route("/about")
 def about(request, response):
     response.text = "Hello from the about page"
+
+@app.route("/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello {name}"
+
+@app.route("/tell/{age:d}")
+def tell_age(request, response, age):
+    response.text = f"Age is {age}"
+
+@app.route("/sum/{num_1:d}/{num_2:d}")
+def sum(request, response, num_1, num_2):
+    total = int(num_1) + int(num_2)
+    response.text = f"{num_1} + {num_2} = {total}"
